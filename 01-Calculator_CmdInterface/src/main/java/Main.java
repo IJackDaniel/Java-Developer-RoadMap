290a90cae925bcdab27c4a9a94effaa318e3dfdd
Danya <danya05af@gmail.com>
2025-09-15 12:23:15 +0300

    Finish project. Version 1.1.0

--- 01-Calculator_CmdInterface/src/main/java/Main.java
+++ 01-Calculator_CmdInterface/src/main/java/Main.java
@@ -5,14 +5,24 @@ public class Main {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
+        String input;
         System.out.printf("Введите первое число: ");
-        calculator.setFirstNumber(scanner.nextDouble());
+        input = scanner.next();
+        if (input.contains(",")) {
+            input = input.replace(",", ".");
+        }
+        calculator.setFirstNumber(Double.parseDouble(input));
 
         System.out.printf("Введите операцию: ");
-        calculator.setOperator(scanner.next());
+        input = scanner.next();
+        calculator.setOperator(input);
 
         System.out.printf("Введите второе число: ");
-        calculator.setSecondNumber(scanner.nextDouble());
+        input = scanner.next();
+        if (input.contains(",")) {
+            input = input.replace(",", ".");
+        }
+        calculator.setSecondNumber(Double.parseDouble(input));
 
         System.out.println("\nРезультат:");
         calculator.solve();
