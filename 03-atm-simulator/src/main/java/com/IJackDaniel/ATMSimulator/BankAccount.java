da5af59f205abd4fea9acaaa9e056f958139be07
Danya <danya05af@gmail.com>
2025-09-24 10:39:25 +0300

    Add login and password in BankAccount class

--- 03-atm-simulator/src/main/java/com/IJackDaniel/ATMSimulator/BankAccount.java
+++ 03-atm-simulator/src/main/java/com/IJackDaniel/ATMSimulator/BankAccount.java
@@ -2,9 +2,24 @@ package com.IJackDaniel.ATMSimulator;
 
 public class BankAccount {
     private double balance;
+    private String login;
+    private String password;
 
     public BankAccount() {
         balance = 100;
+        login = "User";
+        password = "12345";
+    }
+
+    public BankAccount(double balance, String login, String password) {
+        this.balance = balance;
+        this.login = login;
+        this.password = password;
+    }
+
+    // Check login and password
+    public boolean checkLoginAndPassword(String login, String password) {
+        return (login.equals(this.login) && password.equals(this.password));
     }
 
     // Setters
