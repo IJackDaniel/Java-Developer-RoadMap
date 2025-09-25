45d92cd7aa8154d06116f46dc16dc391aa3ec16f
Danya <danya05af@gmail.com>
2025-09-25 14:11:00 +0300

    Add class Task

--- 04-task-manager-cli/src/main/java/com/IJackDaniel/TaskManagerCLI/Task.java
+++ 04-task-manager-cli/src/main/java/com/IJackDaniel/TaskManagerCLI/Task.java
@@ -1,4 +1,29 @@
 package com.IJackDaniel.TaskManagerCLI;
 
 public class Task {
+    private int id;
+    private String description;
+    private boolean status;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.status = false;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    // Setters
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        String statusString = this.status? "[X] " : "[ ] ";
+        return this.id + ". " + statusString + this.description;
+    }
 }
