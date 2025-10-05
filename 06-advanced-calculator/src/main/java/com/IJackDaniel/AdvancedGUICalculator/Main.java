385815af9bdf762396eef90debf51870d4166c92
Danya <danya05af@gmail.com>
2025-10-05 14:44:28 +0300

    First launch. Add id for TextFields. Create Main and controller

--- 06-advanced-calculator/src/main/java/com/IJackDaniel/AdvancedGUICalculator/Main.java
+++ 06-advanced-calculator/src/main/java/com/IJackDaniel/AdvancedGUICalculator/Main.java
@@ -1,4 +1,23 @@
 package com.IJackDaniel.AdvancedGUICalculator;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/IJackDaniel/AdvancedGUICalculator/view/CalculatorView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- 06-advanced-calculator/src/main/java/com/IJackDaniel/AdvancedGUICalculator/controller/CalculatorController.java
+++ 06-advanced-calculator/src/main/java/com/IJackDaniel/AdvancedGUICalculator/controller/CalculatorController.java
@@ -1,4 +1,20 @@
 package com.IJackDaniel.AdvancedGUICalculator.controller;
 
+import com.IJackDaniel.AdvancedGUICalculator.model.CalculatorModel;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
 public class CalculatorController {
+    CalculatorModel model;
+
+    @FXML
+    TextField resultLabel;
+
+    @FXML
+    public void initialize() {
+        model = new CalculatorModel();
+        resultLabel.setText("0");
+        System.out.println("Контроллер инициализирован!");
+    }
 }
