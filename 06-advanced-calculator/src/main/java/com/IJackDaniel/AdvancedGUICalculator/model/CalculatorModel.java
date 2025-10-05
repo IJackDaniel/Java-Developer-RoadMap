186076e75cc8b6e5388ab86f8c57a86b2f199666
Danya <danya05af@gmail.com>
2025-10-05 15:47:58 +0300

    Copy code for moder from previous project

--- 06-advanced-calculator/src/main/java/com/IJackDaniel/AdvancedGUICalculator/model/CalculatorModel.java
+++ 06-advanced-calculator/src/main/java/com/IJackDaniel/AdvancedGUICalculator/model/CalculatorModel.java
@@ -1,4 +1,115 @@
 package com.IJackDaniel.AdvancedGUICalculator.model;
 
 public class CalculatorModel {
+    private double accumulator;
+    private double current;
+    private String operation;
+    private final int accuracy = 5;
+    private boolean shouldResetOnNextInput;
+
+    public CalculatorModel() {
+        reset();
+    }
+
+    public void reset() {
+        this.accumulator = 0.0;
+        this.current = 0.0;
+        this.operation = "";
+        shouldResetOnNextInput = false;
+    }
+
+    private void resetOperation() {
+        this.operation = "";
+    }
+
+    private void shiftValue() {
+        this.accumulator = this.current;
+        this.current = 0;
+    }
+
+    public void evaluate() {
+        if (this.operation.isEmpty()) return;
+
+        switch (operation) {
+            case "+":
+                addition();
+                break;
+            case "-":
+                subtraction();
+                break;
+            case "*":
+                multiplication();
+                break;
+            case "/":
+                if (divisionByZero()) throw new ArithmeticException("Деление на ноль!");
+                division();
+                break;
+        }
+        shouldResetOnNextInput = true;
+    }
+
+    private double round(double value){
+        double scale = Math.pow(10, getAccuracy());
+        return Math.round(value * scale) / scale;
+    }
+
+    public void addition() {
+        double result = round(this.accumulator + this.current);
+        resetOperation();
+        this.current = result;
+    }
+
+    public void subtraction() {
+        double result = round(this.accumulator - this.current);
+        resetOperation();
+        this.current = result;
+    }
+
+    public void multiplication() {
+        double result = round(this.accumulator * this.current);
+        resetOperation();
+        this.current = result;
+    }
+
+    public void division() {
+        double result = round(this.accumulator / this.current);
+        resetOperation();
+        this.current = result;
+    }
+
+    private boolean divisionByZero(){
+        return this.current == 0.0;
+    }
+
+    // Getters
+    public int getAccuracy() {
+        return this.accuracy;
+    }
+
+    public double getCurrent() {
+        return this.current;
+    }
+
+    public String getOperation() {
+        return this.operation;
+    }
+
+    // Setters
+    public void inputDigit(int digit) {
+        if (shouldResetOnNextInput) {
+            shouldResetOnNextInput = false;
+            reset();
+            this.current = digit;
+        } else {
+            this.current = this.current * 10 + digit;
+        }
+
+    }
+
+    public void inputOperation(String operation) {
+        if (!operation.isEmpty()) evaluate();
+        if (shouldResetOnNextInput) shouldResetOnNextInput = false;
+        this.operation = operation;
+        shiftValue();
+    }
 }
